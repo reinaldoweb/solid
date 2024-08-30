83e11144d294f35443fde7b593c0165d1c785c75
reinaldoweb <reinaldoweb@hotmail.com>
2024-08-30 10:30:12 -0300

    Teste de Asserções

--- app_carrinho_compras_b/test/ItemTest.php
+++ app_carrinho_compras_b/test/ItemTest.php
@@ -1,11 +1,57 @@
 <?php
 use PHPUnit\Framework\TestCase;
+
+use src\Item;
 class ItemTest extends TestCase
 {
-    public function testEstouFuncionando()
+    public function testEstaddoInicialItem()
     {
-        $valor = 10;
+        $item = new Item();
 
-        $this->assertEquals($valor, 10);
+        //asserções do PHPUnit
+        $this->assertEquals($item->getdescricao(), '');
+        $this->assertEquals($item->getvalor(), 0);
     }
-}
+
+    public function testGetDescricao()
+    {
+        $descricao = "Cadeira de plástico";
+
+        $item = new Item();
+        $item->setdescricao($descricao);
+
+        $this->assertEquals($item->getdescricao(), $descricao);
+    }
+
+    public function testGetSerValor()
+    {
+        $valor = 35.42;
+
+        $item = new Item();
+        $item->setvalor($valor);
+
+        $this->assertEquals($item->getvalor(), $valor);
+    }
+
+
+    public function testItemValido()
+    {
+        $item = new Item();
+        //Submeter um item válido para o teste e retornar true
+        $item->setvalor(55);
+        $item->setdescricao("Cadeira de plastico");
+        $this->assertTrue($item->itemValido(), false);
+
+        //Submeter um item válido para o teste e retornar false
+        $item->setvalor(0);
+        $item->setdescricao("Cadeira de plastico");
+        $this->assertFalse($item->itemValido(), false);
+
+        //
+
+        $item->setvalor(0);
+        $item->setdescricao("");
+        $this->assertFalse($item->itemValido(), false);
+    }
+
+}//Fim da classe
