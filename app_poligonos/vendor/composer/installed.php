f51200ef159a2f3a85a9a0bbf6d1e366f796949d
reinaldoweb <reinaldoweb@hotmail.com>
2024-09-02 15:17:27 -0300

    Iniciando projeto Poligonos-Principio de substituição de Liskov

---
+++ app_poligonos/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'r1web/poligonos',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e1d94eec8abb39f211661debf225a37f57c484d6',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'r1web/poligonos' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e1d94eec8abb39f211661debf225a37f57c484d6',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
