1e9218a413d46ca6ce74d403d336b807301f30cd
reinaldoweb <reinaldoweb@hotmail.com>
2024-09-03 10:34:27 -0300

    Projeto Polígonos - Testando a Substituição de Tipos

--- app_poligonos/index.php
+++ app_poligonos/index.php
@@ -19,3 +19,12 @@ $quadrado->setAltura(5);
 //$quadrado->setLargura(10);
 
 echo '<h3>Área do Quadrado: '.$quadrado->getArea().'</h3>';
+
+
+
+$retangulo = new Quadrado();
+
+$retangulo->setAltura(5);
+$retangulo->setLargura(10);
+
+echo '<h3>LSP - Área do retangulo: '.$retangulo->getArea().'</h3>';
